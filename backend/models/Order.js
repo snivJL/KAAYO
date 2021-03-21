59e484b8c20da9824aa06c2b5402e0c7e59cf63d
snivJL <julien.lejay@gmail.com>
2021-03-21 19:11:28 +0700

    order model improved

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -4,11 +4,6 @@ const orderSchema = mongoose.Schema(
   {
     userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     products: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
-    status: {
-      type: String,
-      enum: ["pending", "paid", "delivering", "delivered"],
-      default: "paid",
-    },
     shipping: {
       address: { type: String, required: true },
       city: { type: String, required: true },
@@ -18,6 +13,18 @@ const orderSchema = mongoose.Schema(
       country: { type: String, required: true },
     },
     total: { type: Number, default: 0 },
+    paymentMethod: { type: String, required: true },
+    paymentResults: {
+      id: { type: String },
+      status: { type: String },
+      update_time: { type: String },
+      email: { type: String },
+    },
+    shippingPrice: { type: Number, default: 0 },
+    isPaid: { type: Boolean, required: true, default: false },
+    paidAt: { type: Date },
+    isDelivered: { type: Boolean, required: true, default: false },
+    deliveredAt: { type: Date },
     isDeleted: { type: Boolean, default: false },
   },
   { timestamps: true }
