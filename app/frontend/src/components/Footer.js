c88dc25f2f6be8a6185e1db083b3dc67dce6e3f9
snivJL <julien.lejay@gmail.com>
2021-04-02 12:52:39 +0700

    footer responsive

--- app/frontend/src/components/Footer.js
+++ app/frontend/src/components/Footer.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../images/logo.png";
 
 const Footer = () => {
   return (
-    <div className="bg-green-600 h-24 px-4 grid grid-cols-3 items-center text-gray-100">
-      <div className="text-xl">
+    <div className="bg-green-600 h-24 px-4 grid grid-cols-1 grid-rows-2 md:grid-rows-1 md:grid-cols-3  md:pl-18  justify-center items-center text-gray-100">
+      {/* <div className="text-xl">
         <img className="w-32" src={logo} alt="" />
-      </div>
-      <ul className="flex space-x-2 font-light items-center justify-self-center">
+      </div> */}
+      <ul className="flex space-x-2 md:col-span-2 md:w-fullmd:justify-around font-light items-center justify-self-center">
         <Link to="/about">
           <li className="hover:text-gray-400 border-r border-green-600 pr-2 ">
             About Us
@@ -19,26 +18,30 @@ const Footer = () => {
             Privacy Policy
           </li>{" "}
         </Link>
-        <a
-          href="https://www.facebook.com/ka.a.yohandcrafted/?ref=page_internal"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <li className="hover:text-gray-400 ">
-            <i className="fab fa-facebook-square fa-2x "></i>
-          </li>
-        </a>
-        <a
-          href="https://www.instagram.com/ka.a.yo_handcrafted/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <li className="hover:text-gray-400">
-            <i className="fab fa-instagram fa-2x"></i>
-          </li>
-        </a>
+        <div className="flex space-x-2">
+          <a
+            href="https://www.facebook.com/ka.a.yohandcrafted/?ref=page_internal"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <li className="hover:text-gray-400 ">
+              <i className="fab fa-facebook-square fa-2x "></i>
+            </li>
+          </a>
+          <a
+            href="https://www.instagram.com/ka.a.yo_handcrafted/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <li className="hover:text-gray-400">
+              <i className="fab fa-instagram fa-2x"></i>
+            </li>
+          </a>
+        </div>
       </ul>
-      <div className="justify-self-end">&copy; 2021 - KA.A.YO Handcrafted </div>
+      <div className="justify-self-center md:justify-self-end md:ml-auto">
+        &copy; 2021 - KA.A.YO Handcrafted
+      </div>
     </div>
   );
 };
